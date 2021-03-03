df43147bb047dfadee34100246b79ee8774ebb0d
Vitaly Zaitsev <vitaly@easycoding.org>
2021-03-03 19:48:46 +0100

    Extracted variables to a separate module.

---
+++ cmake/Variables.cmake
@@ -0,0 +1,10 @@
+#
+# SPDX-FileCopyrightText: 2021 EasyCoding Team and contributors
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+set(SCHEME_NAME "org.easycoding.TunedSwitcher")
+set(APPLET_NAME "${CMAKE_PROJECT_NAME}-applet")
+set(LIB_NAME "${CMAKE_PROJECT_NAME}-lib")
+set(GUI_NAME "${CMAKE_PROJECT_NAME}-gui")
