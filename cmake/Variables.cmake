86f9d22a11173c3274e6e3c63693dad731999bd5
Vitaly Zaitsev <vitaly@easycoding.org>
2021-03-12 15:02:31 +0100

    Added more variables.

--- cmake/Variables.cmake
+++ cmake/Variables.cmake
@@ -4,8 +4,18 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
+# Reverse-domain schema...
 set(SCHEME_NAME "org.easycoding.TunedSwitcher")
+
+# Internal names...
 set(APPLET_NAME "${CMAKE_PROJECT_NAME}-applet")
 set(LIB_NAME "${CMAKE_PROJECT_NAME}-lib")
 set(GUI_NAME "${CMAKE_PROJECT_NAME}-gui")
 set(I18N_NAME "${CMAKE_PROJECT_NAME}-i18n")
+
+# Desktop files...
+set(APPLET_LAUNCHER "${SCHEME_NAME}.desktop")
+set(GUI_LAUNCHER "${SCHEME_NAME}.Applet.desktop")
+
+# Metainfo files...
+set(APPDATA_MANIFEST "${SCHEME_NAME}.metainfo.xml")
