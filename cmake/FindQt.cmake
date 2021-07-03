e9d820eb54ddf03dc194b8749bfdcd5e51fa3cb4
Vitaly Zaitsev <vitaly@easycoding.org>
2021-07-03 16:05:30 +0200

    Performed autoformat of the FindQt.cmake file.

--- cmake/FindQt.cmake
+++ cmake/FindQt.cmake
@@ -4,4 +4,12 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-find_package(Qt5 5.7.0 COMPONENTS Widgets Core Gui DBus LinguistTools REQUIRED)
+find_package(Qt5 5.7.0
+    COMPONENTS
+        Widgets
+        Core
+        Gui
+        DBus
+        LinguistTools
+    REQUIRED
+)
