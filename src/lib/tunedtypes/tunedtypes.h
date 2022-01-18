acebd114fea55bbb1981e40f319e17862ad55b76
Vitaly Zaitsev <vitaly@easycoding.org>
2022-01-18 11:04:35 +0100

    Updated copyright year.

--- src/lib/tunedtypes/tunedtypes.h
+++ src/lib/tunedtypes/tunedtypes.h
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: 2021 EasyCoding Team and contributors
+ * SPDX-FileCopyrightText: 2021-2022 EasyCoding Team and contributors
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
 */
