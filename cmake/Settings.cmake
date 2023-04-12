8c018f347885ce060193b831980ce94bb5808f59
Vitaly Zaitsev <vitaly@easycoding.org>
2023-04-12 13:16:49 +0200

    Performed major CMake refactoring.

--- cmake/Settings.cmake
+++
@@ -1,9 +0,0 @@
-#
-# SPDX-FileCopyrightText: 2021-2023 EasyCoding Team and contributors
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
-
-option(BUILD_DOC "Build documentation" OFF)
-option(BUILD_MANPAGE "Build manpage" OFF)
-option(BUILD_TESTS "Build tests" OFF)
