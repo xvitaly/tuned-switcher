9fadbb8b6094527f0656dfc3d32b54124701ef8a
Vitaly <vitaly@easycoding.org>
2025-04-17 14:22:11 +0200

    Added a new private method exitApplication() to the TrayIcon class.

--- src/app/trayicon/trayicon.cpp
+++ src/app/trayicon/trayicon.cpp
@@ -63,7 +63,7 @@ void TrayIcon::tryToStartTuned()
     else
     {
         notifications -> ShowNotification(tr("Startup error"), tr("Cannot start Tuned service via D-Bus call. Terminating."));
-        exit(EXIT_FAILURE);
+        exitApplication();
     }
 }
 
@@ -108,6 +108,11 @@ void TrayIcon::setAutoProfileMode(bool mode)
 void TrayIcon::markAutoProfileMode()
 {
     setAutoProfileMode(tunedManager -> IsProfileModeAuto());
+}
+
+void TrayIcon::exitApplication()
+{
+    exit(EXIT_FAILURE);
 }
 
 void TrayIcon::profileChangedEvent(const QString& profile, const bool result, const QString& message)
