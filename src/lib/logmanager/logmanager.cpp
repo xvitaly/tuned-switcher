5e3f61bae97a138035581f517378f0c1c82a2913
Vitaly <vitaly@easycoding.org>
2025-04-21 12:26:13 +0200

    Added a new LogManager class.

---
+++ src/lib/logmanager/logmanager.cpp
@@ -0,0 +1,21 @@
+/**
+ * SPDX-FileCopyrightText: 2021-2025 EasyCoding Team and contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#ifndef LOGMANAGER_H
+#define LOGMANAGER_H
+
+/**
+ * @file logmanager/logmanager.cpp
+ * Contains the LogManager implementation.
+*/
+
+#include <QLoggingCategory>
+
+#include "logmanager/logmanager.h"
+
+Q_LOGGING_CATEGORY(logdbus, "tuned-switcher.dbus")
+
+#endif // LOGMANAGER_H
