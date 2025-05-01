01203ee750cfa0cc59a0e6cc129e8498145e5e45
Vitaly <vitaly@easycoding.org>
2025-05-01 12:23:32 +0200

    Updated the i18n_update.sh helper script to add preamble to all processed files.

--- assets/i18n/i18n_update.sh
+++ assets/i18n/i18n_update.sh
@@ -9,3 +9,7 @@ set -e
 lupdate-qt6 \
     -locations relative \
     ../../src/ -ts tuned-switcher_*.ts -no-obsolete
+
+sed \
+    -e "2i <!--\nSPDX-FileCopyrightText: 2021-2025 EasyCoding Team and contributors\nSPDX-License-Identifier: GPL-3.0-or-later\n-->" \
+    -i tuned-switcher_*.ts
