592cda16de5977b3354704ccd9181b74fda2db03
Vitaly <vitaly@easycoding.org>
2025-05-06 10:52:26 +0200

    Adjusted code style in tunedtypes.h.

--- src/lib/tunedtypes/tunedtypes.h
+++ src/lib/tunedtypes/tunedtypes.h
@@ -41,12 +41,12 @@ struct QTunedProfile
     /**
      * Operator << of the QTunedProfile structure.
     */
-    friend QDBusArgument &operator<<(QDBusArgument &argument, const QTunedProfile &arg);
+    friend QDBusArgument& operator <<(QDBusArgument& argument, const QTunedProfile& arg);
 
     /**
      * Operator >> of the QTunedProfile structure.
     */
-    friend const QDBusArgument &operator>>(const QDBusArgument &argument, QTunedProfile &arg);
+    friend const QDBusArgument& operator >>(const QDBusArgument& argument, QTunedProfile& arg);
 };
 Q_DECLARE_METATYPE(QTunedProfile)
 
@@ -79,12 +79,12 @@ struct QTunedProfileMode
     /**
      * Operator << of the QTunedProfileMode structure.
     */
-    friend QDBusArgument &operator<<(QDBusArgument &argument, const QTunedProfileMode &arg);
+    friend QDBusArgument& operator <<(QDBusArgument& argument, const QTunedProfileMode& arg);
 
     /**
      * Operator >> of the QTunedProfileMode structure.
     */
-    friend const QDBusArgument &operator>>(const QDBusArgument &argument, QTunedProfileMode &arg);
+    friend const QDBusArgument& operator >>(const QDBusArgument& argument, QTunedProfileMode& arg);
 };
 Q_DECLARE_METATYPE(QTunedProfileMode)
 
@@ -121,12 +121,12 @@ struct QTunedResult
     /**
      * Operator << of the QTunedResult structure.
     */
-    friend QDBusArgument &operator<<(QDBusArgument &argument, const QTunedResult &arg);
+    friend QDBusArgument& operator <<(QDBusArgument& argument, const QTunedResult& arg);
 
     /**
      * Operator >> of the QTunedResult structure.
     */
-    friend const QDBusArgument &operator>>(const QDBusArgument &argument, QTunedResult &arg);
+    friend const QDBusArgument& operator >>(const QDBusArgument& argument, QTunedResult& arg);
 };
 Q_DECLARE_METATYPE(QTunedResult)
 
