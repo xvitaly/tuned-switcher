ef6aaf259eb73249cbdf209869ad5e4b8091d44c
Vitaly <vitaly@easycoding.org>
2025-05-09 10:00:29 +0200

    Updated constructor of the MainWindow class declaration.

--- src/app/mainwindow/mainwindow.cpp
+++ src/app/mainwindow/mainwindow.cpp
@@ -31,9 +31,7 @@
 #include "tunedmanager/tunedmanager.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget* parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui -> setupUi(this);
     loadSettings();
