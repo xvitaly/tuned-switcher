7448e1bbf4b3085c5b46092697161202fd91cc1c
Vitaly <vitaly@easycoding.org>
2025-05-13 10:29:18 +0200

    Create the TrayIcon object in stack to prevent memory leaks.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -39,8 +39,8 @@ int main(int argc, char* argv[])
 
     if (QSystemTrayIcon::isSystemTrayAvailable() && !qEnvironmentVariableIsSet("TUNED_SWITCHER_FORCE_GUI"))
     {
-        TrayIcon* trayIcon = new TrayIcon();
-        trayIcon -> Show();
+        TrayIcon trayIcon;
+        trayIcon.Show();
     }
     else
     {
