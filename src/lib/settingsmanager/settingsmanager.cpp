154660faacf6fd6ea2a611c74943fd8ed103afd4
Vitaly <vitaly@easycoding.org>
2025-09-25 13:52:00 +0200

    Added implementation to the SettingsManager class.

--- src/lib/settingsmanager/settingsmanager.cpp
+++ src/lib/settingsmanager/settingsmanager.cpp
@@ -9,10 +9,49 @@
  * Contains the SettingsManager class implementation.
 */
 
+#include <QByteArray>
 #include <QObject>
+#include <QSettings>
+#include <QStringLiteral>
 
 #include "settingsmanager/settingsmanager.h"
 
+QByteArray SettingsManager::GetGeometry() const
+{
+    return settings -> value(QStringLiteral("widget/geometry")).toByteArray();
+}
+
+void SettingsManager::SetGeometry(const QByteArray value)
+{
+    settings -> setValue(QStringLiteral("widget/geometry"), value);
+}
+
+QByteArray SettingsManager::GetWindowState() const
+{
+    return settings -> value(QStringLiteral("widget/windowState")).toByteArray();
+}
+
+void SettingsManager::SetWindowState(const QByteArray value)
+{
+    settings -> setValue(QStringLiteral("widget/windowState"), value);
+}
+
+bool SettingsManager::GetSoundEnabled() const
+{
+    return settings -> value(QStringLiteral("widget/soundEnabled"), true).toBool();
+}
+
+void SettingsManager::SetSoundEnabled(const bool value)
+{
+    settings -> setValue(QStringLiteral("widget/soundEnabled"), value);
+}
+
+void SettingsManager::Save()
+{
+    settings -> sync();
+}
+
 SettingsManager::SettingsManager(QObject* parent) : QObject(parent)
 {
+    settings = new QSettings(this);
 }
--- src/lib/settingsmanager/settingsmanager.h
+++ src/lib/settingsmanager/settingsmanager.h
@@ -12,7 +12,9 @@
  * Contains the SettingsManager class definition.
 */
 
+#include <QByteArray>
 #include <QObject>
+#include <QSettings>
 
 /**
  * Class for working with program settings.
@@ -21,11 +23,20 @@ class SettingsManager : public QObject
 {
     Q_OBJECT
 public:
+    QByteArray GetGeometry() const;
+    void SetGeometry(const QByteArray);
+    QByteArray GetWindowState() const;
+    void SetWindowState(const QByteArray);
+    bool GetSoundEnabled() const;
+    void SetSoundEnabled(const bool);
+    void Save();
     /**
      * Main constructor of the SettingsManager class.
      * @param parent Parent.
     */
     explicit SettingsManager(QObject* parent = nullptr);
+private:
+    QSettings* settings;
 };
 
 #endif // SETTINGSMANAGER_H
