bdaa302fe78652e80e2fee43effe1fceeaa91d56
Vitaly <vitaly@easycoding.org>
2025-10-22 13:37:52 +0200

    Added localization strings for the new service control features.

--- assets/i18n/tuned-switcher_ru.ts
+++ assets/i18n/tuned-switcher_ru.ts
@@ -8,17 +8,17 @@ SPDX-License-Identifier: GPL-3.0-or-later
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../src/app/mainwindow/mainwindow.ui" line="+73"/>
+        <location filename="../../src/app/mainwindow/mainwindow.ui" line="+76"/>
         <source>Active profile:</source>
         <translation>Текущий профиль:</translation>
     </message>
     <message>
-        <location line="+34"/>
+        <location line="+50"/>
         <source>Auto-select profile</source>
         <translation>Авто-выбор профиля</translation>
     </message>
     <message>
-        <location filename="../../src/app/mainwindow/mainwindow.cpp" line="+124"/>
+        <location filename="../../src/app/mainwindow/mainwindow.cpp" line="+139"/>
         <source>Cannot start Tuned service via D-Bus call. Terminating.</source>
         <translation>Не удалось запустить сервис Tuned при помощи D-Bus. Приложение будет завершено.</translation>
     </message>
@@ -33,7 +33,48 @@ SPDX-License-Identifier: GPL-3.0-or-later
         <translation>Сервис Tuned не запущен. Хотите ли вы запустить его прямо сейчас?</translation>
     </message>
     <message>
-        <location line="+85"/>
+        <location line="+33"/>
+        <location line="+71"/>
+        <source>Service control</source>
+        <translation>Управление сервисом</translation>
+    </message>
+    <message>
+        <location line="-69"/>
+        <source>Enable the service</source>
+        <translation>Запустить сервис</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Disable the service</source>
+        <translation>Остановить сервис</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Reload the service</source>
+        <translation>Перезагрузить сервис</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Shut down the service</source>
+        <translation>Выключить сервис</translation>
+    </message>
+    <message>
+        <location line="+57"/>
+        <source>The requested service control operation completed successfully.</source>
+        <translation>Выбранная операция по управлению сервисом была успешно выполнена.</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Service control error</source>
+        <translation>Ошибка управления сервисом</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Failed to perform the requested service control operation! Current settings remain unchanged.</source>
+        <translation>Не удалось выполнить запрошенную операцию по управлению сервисом! Текущие настройки не были изменены.</translation>
+    </message>
+    <message>
+        <location line="+17"/>
         <source>The active profile was automatically switched to &lt;b&gt;%1&lt;/b&gt;.</source>
         <translation>Текущий профиль был автоматически переключён на &lt;b&gt;%1&lt;/b&gt;.</translation>
     </message>
@@ -78,7 +119,7 @@ SPDX-License-Identifier: GPL-3.0-or-later
 <context>
     <name>TrayIcon</name>
     <message>
-        <location filename="../../src/app/trayicon/trayicon.cpp" line="+60"/>
+        <location filename="../../src/app/trayicon/trayicon.cpp" line="+68"/>
         <source>Startup error</source>
         <translation>Ошибка запуска</translation>
     </message>
@@ -109,12 +150,38 @@ SPDX-License-Identifier: GPL-3.0-or-later
     </message>
     <message>
         <location line="+6"/>
-        <location line="+65"/>
+        <location line="+90"/>
         <source>Profile switch error</source>
         <translation>Ошибка переключения</translation>
     </message>
     <message>
-        <location line="-58"/>
+        <location line="-83"/>
+        <location line="+90"/>
+        <source>Service control</source>
+        <translation>Управление сервисом</translation>
+    </message>
+    <message>
+        <location line="-88"/>
+        <source>Enable the service</source>
+        <translation>Запустить сервис</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Disable the service</source>
+        <translation>Остановить сервис</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Reload the service</source>
+        <translation>Перезагрузить сервис</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Shut down the service</source>
+        <translation>Выключить сервис</translation>
+    </message>
+    <message>
+        <location line="+10"/>
         <source>Active profile</source>
         <translation>Текущий профиль</translation>
     </message>
@@ -129,7 +196,7 @@ SPDX-License-Identifier: GPL-3.0-or-later
         <translation>Авто-выбор профиля</translation>
     </message>
     <message>
-        <location line="+21"/>
+        <location line="+22"/>
         <source>Profile auto-selection error</source>
         <translation>Ошибка авто-выбора профиля</translation>
     </message>
@@ -139,7 +206,22 @@ SPDX-License-Identifier: GPL-3.0-or-later
         <translation>Не удалось переключить профиль из-за ошибки: %1</translation>
     </message>
     <message>
-        <location line="-10"/>
+        <location line="+7"/>
+        <source>The requested service control operation completed successfully.</source>
+        <translation>Выбранная операция по управлению сервисом была успешно выполнена.</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Service control error</source>
+        <translation>Ошибка управления сервисом</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Failed to perform the requested service control operation! Current settings remain unchanged.</source>
+        <translation>Не удалось выполнить запрошенную операцию по управлению сервисом! Текущие настройки не были изменены.</translation>
+    </message>
+    <message>
+        <location line="-19"/>
         <source>Failed to enable profile auto-selection: %1</source>
         <translation>Не удалось активировать авто-выбор профиля из-за ошибки: %1</translation>
     </message>
