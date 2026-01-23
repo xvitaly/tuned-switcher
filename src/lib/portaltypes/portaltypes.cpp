04c2186b38d4f596260ee2cacdd6c3c0c74a0680
Vitaly <vitaly@easycoding.org>
2026-01-23 09:56:57 +0100

    Added a new collection of structures for working with portals.

---
+++ src/lib/portaltypes/portaltypes.cpp
@@ -0,0 +1,14 @@
+/**
+ * SPDX-FileCopyrightText: 2021-2026 EasyCoding Team and contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+/**
+ * @file portaltypes/portaltypes.cpp
+ * Contains implementations of special structures for working with portals.
+*/
+
+#include "portaltypes/portaltypes.h"
+
+//
---
+++ src/lib/portaltypes/portaltypes.h
@@ -0,0 +1,19 @@
+/**
+ * SPDX-FileCopyrightText: 2021-2026 EasyCoding Team and contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#ifndef PORTALTYPES_H
+#define PORTALTYPES_H
+
+/**
+ * @file portaltypes/portaltypes.h
+ * Contains definitions of special structures for working with portals.
+*/
+
+#include <QObject>
+
+//
+
+#endif // PORTALTYPES_H
