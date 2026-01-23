bd93ab9196e67e7067602048e232a7c65a1ee481
Vitaly <vitaly@easycoding.org>
2026-01-23 10:06:29 +0100

    Added a new structure for working with portal DBus responses.

--- src/lib/portaltypes/portaltypes.cpp
+++ src/lib/portaltypes/portaltypes.cpp
@@ -9,6 +9,26 @@
  * Contains implementations of special structures for working with portals.
 */
 
+#include <QDBusArgument>
+
 #include "portaltypes/portaltypes.h"
 
-//
+QDBusArgument& operator <<(QDBusArgument& argument, const QRequestResponse& response)
+{
+    argument.beginStructure();
+    argument << response.Code;
+    argument << response.Data;
+    argument.endStructure();
+
+    return argument;
+}
+
+const QDBusArgument& operator >>(const QDBusArgument& argument, QRequestResponse& response)
+{
+    argument.beginStructure();
+    argument >> response.Code;
+    argument >> response.Data;
+    argument.endStructure();
+
+    return argument;
+}
--- src/lib/portaltypes/portaltypes.h
+++ src/lib/portaltypes/portaltypes.h
@@ -12,8 +12,19 @@
  * Contains definitions of special structures for working with portals.
 */
 
+#include <QDBusArgument>
+#include <QMetaType>
 #include <QObject>
+#include <QVariantMap>
 
-//
+struct QRequestResponse
+{
+    unsigned int Code;
+    QVariantMap Data;
+    QRequestResponse() : Code(), Data() {}
+    friend QDBusArgument& operator <<(QDBusArgument&, const QRequestResponse&);
+    friend const QDBusArgument& operator >>(const QDBusArgument&, QRequestResponse&);
+};
+Q_DECLARE_METATYPE(QRequestResponse)
 
 #endif // PORTALTYPES_H
