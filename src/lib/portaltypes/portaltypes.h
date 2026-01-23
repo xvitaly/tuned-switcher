acbd4d3ecc1be259e841947f633162db4b5391db
Vitaly <vitaly@easycoding.org>
2026-01-23 10:13:48 +0100

    Added the inline documentation to the structure for working with portal responses.

--- src/lib/portaltypes/portaltypes.h
+++ src/lib/portaltypes/portaltypes.h
@@ -17,12 +17,38 @@
 #include <QObject>
 #include <QVariantMap>
 
+/**
+ * Structure for working with the portal request responses.
+*/
 struct QRequestResponse
 {
+    /**
+     * Response code: 0 - success; 1 - cancelled; 2 - timeout.
+    */
     unsigned int Code;
+
+    /**
+     * Results data.
+    */
     QVariantMap Data;
+
+    /**
+     * Constructor of the QRequestResponse structure.
+    */
     QRequestResponse() : Code(), Data() {}
+
+    /**
+     * Operator << of the QRequestResponse structure.
+     * @param argument Instance of the QDBusArgument class.
+     * @param profile Instance of the QRequestResponse class.
+    */
     friend QDBusArgument& operator <<(QDBusArgument&, const QRequestResponse&);
+
+    /**
+     * Operator >> of the QRequestResponse structure.
+     * @param argument Instance of the QDBusArgument class.
+     * @param profile Instance of the QRequestResponse class.
+    */
     friend const QDBusArgument& operator >>(const QDBusArgument&, QRequestResponse&);
 };
 Q_DECLARE_METATYPE(QRequestResponse)
