1aabbbef742f5120d3944a01b315b4c2921a6981
Vitaly <vitaly@easycoding.org>
2026-01-27 08:52:31 +0100

    Updated the inline documentation of various public methods of the AutorunFile class.

--- src/lib/autorunfile/autorunfile.h
+++ src/lib/autorunfile/autorunfile.h
@@ -38,24 +38,32 @@ public:
     /**
      * Get whether the autorun feature is enabled.
      * @returns Whether the autorun feature is enabled.
+     * @retval true The autorun feature is enabled.
+     * @retval false The autorun feature is not enabled.
     */
     bool IsEnabled() const override;
 
     /**
      * Get whether the autorun feature is supported on running platform.
-     * @returns Whether the autorun feature is supported.
+     * @returns Whether the autorun feature is supported on running platform.
+     * @retval true The autorun feature is supported on running platform.
+     * @retval false The autorun feature is not supported on running platform.
     */
     bool IsSupported() const override;
 
     /**
      * Enable the autorun feature.
      * @returns Result of the requested operation.
+     * @retval true The requested operation completed successfully.
+     * @retval false An error occurred when trying to perform the requested operation.
     */
     bool Enable() const override;
 
     /**
      * Disable the autorun feature.
      * @returns Result of the requested operation.
+     * @retval true The requested operation completed successfully.
+     * @retval false An error occurred when trying to perform the requested operation.
     */
     bool Disable() const override;
 private:
