f53b002e274920b5442d2b97763662e15130977a
Vitaly <vitaly@easycoding.org>
2026-01-29 09:40:54 +0100

    Fixed build due to incorrect method prototype.

--- src/lib/autorunportal/autorunportal.h
+++ src/lib/autorunportal/autorunportal.h
@@ -146,7 +146,7 @@ private slots:
      * @param response Response code: 0 - success; 1 - cancelled; 2 - timeout..
      * @param results Results data.
     */
-    void RequestResponseEvent(unsigned int, const QVariantMap&);
+    void RequestResponseEvent(unsigned int, const QVariantMap&) const;
 };
 
 #endif // AUTORUNPORTAL_H
