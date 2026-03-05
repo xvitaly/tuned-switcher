e407ccffe50d93c29b6ed3c621f6d5679c660946
Vitaly <vitaly@easycoding.org>
2026-03-05 08:46:05 +0100

    Removed redundant line break from i18n_update.sh helper script.

--- assets/i18n/i18n_update.sh
+++ assets/i18n/i18n_update.sh
@@ -10,8 +10,7 @@ lupdate-qt6 \
     -locations relative \
     ../../src/ -ts tuned-switcher_*.ts -no-obsolete
 
-sed \
-    -r \
+sed -r \
     -e "s/language=\"([a-z]{2,})_([A-Z]{2,})\"/language=\"\1\"/g" \
     -e "2i <!--\nSPDX-FileCopyrightText: 2021-2026 EasyCoding Team and contributors\nSPDX-License-Identifier: GPL-3.0-or-later\n-->" \
     -i tuned-switcher_*.ts
