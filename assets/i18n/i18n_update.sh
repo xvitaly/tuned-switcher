6bb3e471619b0723b0c1827c2a4cf4c5800b1447
Vitaly <vitaly@easycoding.org>
2026-03-05 08:40:19 +0100

    Enabled the extended regular expressions support in i18n_update.sh helper script.

--- assets/i18n/i18n_update.sh
+++ assets/i18n/i18n_update.sh
@@ -11,6 +11,7 @@ lupdate-qt6 \
     ../../src/ -ts tuned-switcher_*.ts -no-obsolete
 
 sed \
+    -r \
     -e "s/language=\"([a-z]{2,})_([A-Z]{2,})\"/language=\"\1\"/g" \
     -e "2i <!--\nSPDX-FileCopyrightText: 2021-2026 EasyCoding Team and contributors\nSPDX-License-Identifier: GPL-3.0-or-later\n-->" \
     -i tuned-switcher_*.ts
