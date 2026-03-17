537dc4ea036af570d012b6f892e2979361526696
Vitaly <vitaly@easycoding.org>
2026-03-17 08:43:18 +0100

    Added a new DebugSettings namespace to the library target.

---
+++ src/lib/debugsettings/debugsettings.cpp
@@ -0,0 +1,12 @@
+/**
+ * SPDX-FileCopyrightText: 2021-2026 EasyCoding Team and contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+/**
+ * @file debugsettings/debugsettings.cpp
+ * Contains the DebugSettings namespace implementation.
+*/
+
+#include "debugsettings/debugsettings.h"
---
+++ src/lib/debugsettings/debugsettings.h
@@ -0,0 +1,22 @@
+/**
+ * SPDX-FileCopyrightText: 2021-2026 EasyCoding Team and contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#ifndef DEBUGSETTINGS_H
+#define DEBUGSETTINGS_H
+
+/**
+ * @file debugsettings/debugsettings.h
+ * Contains the DebugSettings namespace definition.
+*/
+
+/**
+ * Namespace with static methods for working with debug settings.
+*/
+namespace DebugSettings
+{
+}
+
+#endif // DEBUGSETTINGS_H
